330d15a9605ce475c592e4d22650b2fc125f04a9
jeevan-abc <jeevan.raj@abc.courses>
2025-04-18 10:54:30 +0530

    stable version 1

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,8 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <div>
-        Hello form next js with tailwindcss
+        Hello form next js with tailwindcss and typescript
+        testing 1
       </div>
     </div>
   );
